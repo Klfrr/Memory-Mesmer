6a09416bc3bfa3e1496e3fd26eb203032fae9a25
jackyy2 <jackyyu2002@nevada.unr.edu>
2023-02-03 20:54:40 -0800

    Added the database script WIP
    Added the script the is supposed to open the database and write some temp values into it.  Still WIP, have a unreferenced null pointer error.

---
+++ Assets/Scripts/DatabaseTesting.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mono.Data.Sqlite;
+using System.Data;
+using System;
+using System.IO;
+
+
+
+
+public class DatabaseTesting : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Code from https://answers.unity.com/questions/743400/database-sqlite-setup-for-unity.html
+        //Used to build the connection path to the database
+        string dataBaseConn = "URI=file:" + Application.dataPath + "/Database/Database.db"; 
+        
+        //Creates the connection to the database
+        IDbConnection dbconn;
+        dbconn = new SqliteConnection(dataBaseConn);
+        dbconn.Open();
+
+
+        IDbCommand dbcmd = dbconn.CreateCommand(); 
+
+        string sqlQuery = "SELECT User,Score" + "FROM PlaceSequence";
+
+        dbcmd.CommandText = sqlQuery;
+        IDataReader reader = dbcmd.ExecuteReader();
+
+        IDbCommand cmnd = dbconn.CreateCommand();
+        cmnd.CommandText = "INSERT INTO Database (User,Score) VALUES (Temp, 5)";
+        cmnd.ExecuteNonQuery();
+
+        dbconn.Close();
+
+
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
