dbcc2f29da5c7f89e316407aafbaaa11fd5b6c7e
Klfrr <kayjiayi495@gmail.com>
2023-02-18 15:55:43 -0800

    Added Sleek Assets
    No implementation of the assets yet

---
+++ Assets/UI pack/Scripts/FillValueNumber.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//Slider? from Sleek Essential UI Pack
+
+public class FillValueNumber : MonoBehaviour
+{
+    public Image TargetImage;
+    // Update is called once per frame
+    void Update()
+    {
+        float amount = TargetImage.fillAmount * 100;
+        gameObject.GetComponent<Text>().text = amount.ToString("F0");
+    }
+}
